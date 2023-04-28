8bbd6d2043a148b797d53ee039858b8d5427f77c
Dwene Louis <Dwenegt@yahoo.com>
2023-04-28 15:42:26 -0400

    changed the output of the main message

--- src/main/java/org/yup/accountingledger/AccountingLedgerApp.java
+++ src/main/java/org/yup/accountingledger/AccountingLedgerApp.java
@@ -4,7 +4,7 @@ public class AccountingLedgerApp {
 
     public static void main(String[] args){
 
-        System.out.println("anything");
+        System.out.println("This is the new text");
     }
 
 }
